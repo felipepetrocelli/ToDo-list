f701cb104ba046f96bbe51d535f84ea7004da0a2
felipe petrocelli <felipe.petrocelli@donext.com.br>
2022-04-20 11:57:24 -0300

    layout

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
+
 const app = express()
 const port = 8000
 
+const noteRoutes = require('./routes/notes')
+
+// template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 //Rotas
-app.get('/', function(req,res){
-    res.send('Aplicativo Funcionando')
+app.get('/', function (req, res) {
+    res.render('home')
 })
+
+app.use('/notes', noteRoutes)
 
 app.listen(port, () => {
     console.log(`Projeto rodando na porta ${port}`)
