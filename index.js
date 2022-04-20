4a757f89a89f12491988d889dd45b601534b5253
felipe petrocelli <felipe.petrocelli@donext.com.br>
2022-04-20 13:49:33 -0300

    aplicação concluida ver 1.0

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
-
 const app = express()
 const port = 8000
 
+const db = require('./db/conection')
 const noteRoutes = require('./routes/notes')
 
 // template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //Rotas
-app.get('/', function (req, res) {
-    res.render('home')
+app.get('/', async function (req, res) {
+    const notes = await db.getDb().db().collection('notes').find({}).toArray()
+    res.render('home', { notes })
 })
 
 app.use('/notes', noteRoutes)
 
-app.listen(port, () => {
-    console.log(`Projeto rodando na porta ${port}`)
+db.initDb((err, db) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("Banco conectado com sucesso")
+        app.listen(port, () => {
+            console.log(`Projeto rodando na porta ${port}`)
+        })
+    }
 })
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,74 @@
 const Router = require('express').Router
+const db = require('../db/conection')
+const { ObjectId } = require('mongodb')
 
 const router = Router();
+
+router.get('/:id', async function (req, res) {
+
+    const id = new ObjectId(req.params.id);
+
+    const note = (await db.getDb().db().collection('notes').findOne({ _id: id }))
+
+    res.render('notes/detail', { note });
+
+});
+
 
 router.get('/', function (req, res) {
     res.render('notes/create')
 })
+
+router.post('/', async function (req, res) {
+    const data = req.body
+    const title = data.title
+    const description = data.description
+
+   await db.getDb()
+        .db()
+        .collection('notes')
+        .insertOne({ title, description })
+
+    res.redirect(301, '/')
+})
+
+router.post('/delete', async function (req, res) {
+    const data = req.body
+    const id = new ObjectId(data.id);
+
+   await db.getDb()
+        .db()
+        .collection('notes')
+        .deleteOne({ _id: id })
+
+    res.redirect(301, '/')
+})
+
+router.post('/update', async function(req,res){
+    const data = req.body
+    const id = new ObjectId(data.id)
+    const title = data.title
+    const description = data.description
+
+    await db.getDb().db().collection('notes').updateOne({ _id: id },{$set:{title:title, description:description}})
+
+    res.redirect('/')
+
+
+})
+
+router.get('/edit/:id', async function(req,res){
+
+    const id = new ObjectId(req.params.id);
+
+    const note = (await db.getDb().db().collection('notes').findOne({ _id: id }))
+
+    res.render('notes/edit', { note });
+
+})
+
+
+
+
 
 module.exports = router
